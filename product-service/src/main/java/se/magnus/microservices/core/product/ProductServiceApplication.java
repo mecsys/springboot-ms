bf3186383714a1ba349be8a93b11fabd61e912bb
Isaac Mecchi <isaac.mecchi@sensedia.com>
2024-01-22 22:38:02 -0300

    Extend product service.

--- product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,11 @@ package se.magnus.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan("se.magnus")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
